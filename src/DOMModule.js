1c66c9201e65d27988b595cab07098a47d44c475
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-25 09:34:18 +0200

    DOMModule changes

--- src/DOMModule.js
+++ src/DOMModule.js
@@ -1,34 +1,43 @@
 import {GRID} from "./config";
 
-const DOMMOdule = function (playerOne, playerTwo,) {
-    const ENEMY_HALF = 'enemy-half';
-    const HUMAN_HALF = 'human-half';
+const DOMMOdule = function (playerOne, playerTwo) {
 
-    const playerTwoBoard = playerOne.enemyGameBoard;
-    const playerOneBoard = playerTwo.enemyGameBoard;
+    const ONE_HALF = playerOne.enemyGameBoard.container;
+    const SECOND_HALF = playerTwo.enemyGameBoard.container;
+
 
     const init = () => {
-        drawBoard(ENEMY_HALF);
-        drawBoard(HUMAN_HALF);
+        drawBoard(ONE_HALF);
+        drawBoard(SECOND_HALF);
 
-        drawShips(playerOneBoard, HUMAN_HALF);
-        drawShips(playerTwoBoard, ENEMY_HALF);
-
-        attachEvents();
+        drawShips(playerOne, ONE_HALF);
+        drawShips(playerTwo, SECOND_HALF);
 
     }
 
-    const attachEvents = () => {
+    const attachEvents = (boardContainer) => {
         // Only set the event on the opponent board;
-        document.getElementById(ENEMY_HALF).addEventListener('click', function (e) {
+
+        document.getElementById(boardContainer).addEventListener('click', function (e) {
             const position = (e.target.getAttribute('data-position'));
+
             if (position) {
+
                 if (playerTwo.getTurn()) {
-                    playerTwo.play(position);
+                    if (playerTwo.play(position)){
+                        // SO a player can play again, dont toggle places
+                        return;
+                    }
                 }
                 else {
-                    playerOne.play(position);
+                    if(playerOne.play(position)){
+                        return;
+                    }
                 }
+                playerOne.toggleTurn();
+                playerTwo.toggleTurn();
+
+                hideShipsBasedOnTurns();
             }
 
         }, false);
@@ -58,17 +67,63 @@ const DOMMOdule = function (playerOne, playerTwo,) {
             }
             document.getElementById(container).append(row)
         }
+        attachEvents(container);
     }
 
 
-    const drawShips = (gameBoard, container) => {
-        gameBoard.getGameBoard().forEach((ship) => {
-            document.getElementById(container + '-' + (ship.startingPosition)).innerHTML = <div draggable="true" class="ship ${ship.axis} ${ship.axis}-ship-${ship.ship.length}"></div>;
+    const drawShips = (player, container) => {
+
+        const { enemyGameBoard } = player;
+        enemyGameBoard.getGameBoard().forEach((ship) => {
+            document.getElementById(container + '-' + (ship.startingPosition)).innerHTML = <div class="ship ${ship.axis} ${ship.axis}-ship-${ship.ship.length}"></div>;
         });
+        hideShipsBasedOnTurns();
     }
+
+    const hideShipsBasedOnTurns = () => {
+        if (playerOne.getTurn()) {
+            // Hide Player two ships
+            const playerTwoBoard = playerOne.enemyGameBoard;
+            const playerTwoShipsEl = document.querySelectorAll(#${playerTwoBoard.container} div.ship);
+            playerTwoShipsEl.forEach((shipEl) => {
+                shipEl.classList.add('hide-ship');
+            });
+
+
+            // Show Player One ships
+            const playerOneBoard = playerTwo.enemyGameBoard;
+            const playerOneShipsEl = document.querySelectorAll(#${playerOneBoard.container} div.ship);
+            playerOneShipsEl.forEach((shipEl) => {
+                shipEl.classList.remove('hide-ship');
+            });
+
+        }
+        else if (playerTwo.getTurn()) {
+            // Hide Player one ships
+            const playerOneBoard = playerTwo.enemyGameBoard;
+            const playerOneShipsEl = document.querySelectorAll(#${playerOneBoard.container} div.ship);
+            playerOneShipsEl.forEach((shipEl) => {
+                shipEl.classList.add('hide-ship');
+            });
+
+            // Show Player two ships
+            const playerTwoBoard = playerOne.enemyGameBoard;
+            const playerTwoShipsEl = document.querySelectorAll(#${playerTwoBoard.container} div.ship);
+            playerTwoShipsEl.forEach((shipEl) => {
+                shipEl.classList.remove('hide-ship');
+            });
+        }
+    }
+
     return {
         init
     }
 };
 
+export const renderHitBoard = (boardContainer, position) => {
+    document.getElementById(${boardContainer}-${position}).classList.add('hit')
+}
+export const renderMissBoard = (boardContainer, position) => {
+    document.getElementById(${boardContainer}-${position}).classList.add('miss')
+}
 export default DOMMOdule;
